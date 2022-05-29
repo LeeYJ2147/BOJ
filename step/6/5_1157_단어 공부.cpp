69f7248c7a1c77e68bbd421a113db8f3b652fbf5
Yujun Lee <june040929@gmail.com>
2022-05-29 17:40:21 +0900

    second commit

--- step/6/5_1157_단어 공부.cpp
+++ step/6/5_1157_단어 공부.cpp
@@ -1,32 +1,28 @@
-#include <iostream>
-#include <string.h>
+#include <stdio.h>
 
-int DT[27];
+int DT[60];
+char sen[1000001];
 
 int main()
 {
-    char temp;
-    int max=0, check=0;
-    temp=std::cin.get();
-    for(int i=0; temp!='\n' || i<10; i++)
+    int max=0, maxNum=0, check=0;
+    scanf("%s", sen);
+    for(int i=0; sen[i]!=0; ++i)
+        DT[int(sen[i])-65]++;
+    for(int i=0; i<27; i++)
     {
-        if(int(temp)-65>26) DT[int(temp)-26]++;
-        else DT[int(temp)]++;
-        temp=std::cin.get();
-    }
-    for(int i=0; i<26; i++)
-    {
-        if(max<DT[i]) max=DT[i], check=0;
-        else check++;
+        if(max<DT[i]+DT[i+32]) max=DT[i]+DT[i+32], maxNum=i, check=0;
+        else if(max==DT[i]+DT[i+32]) check++;
     }
     if(check) printf("?");
-    else printf("%c", max+65);
+    else printf("%c", maxNum+65);
     return 0;
 }
 
 
 
-/* 시간초과
+/* 시간초과, strlen(str)을 n번 반복할 때마다 처리하므로 오래걸렸을 것.
+// 위 추론이 정확함, i<strlen(str) 말고 int len=어쩌구, i<len했을 때는 시간초과 없음.
 int main()
 {
     char str[1000001];
