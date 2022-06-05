da4d52f8f3666e0b834b4139c81735c047af3b9e
Yujun Lee <june040929@gmail.com>
2022-06-06 00:59:06 +0900

    second commit

--- step/7/4_2869_달팽이는_올라가고_싶다.cpp
+++ step/7/4_2869_달팽이는_올라가고_싶다.cpp
@@ -5,7 +5,6 @@ int main()
 {
     int up, down, goal;
     scanf("%d %d %d", &up, &down, &goal);
-    printf("분자:%d\n분모:%f\n분수값:%f\n올림:%f\nint:%d\n", goal-up, double(up-down), double(goal-up)/double(up-down), ceil(double(goal-up)/double(up-down)), int(double(goal-up)/double(up-down)));
     printf("%d", int(ceil(double(goal-up)/double(up-down)))+1);
     return 0;
 }
